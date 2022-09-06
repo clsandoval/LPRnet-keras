1fd7bf04585a26aab821c8cee4a4dbf707512add
unknown <clsandoval@up.edu.ph>
2022-09-06 22:38:57 +0800

    Finish stn, start testing and visualizing translation network with trivial examples (mnist)

--- LocNet/LocNet.py
+++ LocNet/LocNet.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np, tensorflow as tf, tensorflow.keras as keras, tensorflow.keras.layers as layers
 
 
@@ -12,8 +13,24 @@ To-Do
 """
 
 #
-def build_localization():
-    localization = keras.Sequential([
-
-    ])
+def build_localization_layers(input_layer):
+    x = layers.Conv2D(8,(7,7), padding='same') (input_layer)
+    x = layers.MaxPool2D(pool_size=(2,2),strides=(2,2),padding='same')(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(10,(5,5), padding='same')(x)
+    localization = layers.MaxPool2D(pool_size=(2,2),strides=(2,2),padding='same')(x)
     return localization
+
+
+def build_regressor_layers(input_layer):
+    x = layers.Dense(32, activation='relu')(input_layer)
+    x1 = layers.Dense(6, weights= [np.zeros(shape=(32,6)),np.array([1,0,0,0,1,0])])(x)
+    return x1
+
+input_layer = layers.Input(shape=(24,94,3))
+regressor = build_regressor_layers(input_layer)
+model = keras.Model(inputs=input_layer, outputs=regressor)
+
+# %%
+
+# %%
